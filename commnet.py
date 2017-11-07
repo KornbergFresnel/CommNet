48d26deb647fc4aaf1e1c3c1dd1717b8873b2cfe
Kornberg <kornbergfresnel@outlook.com>
2017-11-07 14:02:29 +0800

    refresh

--- commnet.py
+++ commnet.py
@@ -14,7 +14,11 @@ class CommNet(BaseModel):
         # ==== create network =====
         with tf.variable_scope("CommNet"):
             self.eval_name = tf.get_variable_scope().name
-            self.policy = self._create_network()  # n * 5 * n_actions
+            self.look_up = tf.get_variable("look_up_table", shape=(self.num_agents, self.vector_len),
+                                      initializer=tf.random_normal_initializer)
+            self.dense_weight = tf.get_variable("dense_w", shape=(self.vector_len, self.n_actions),
+                                                initializer=tf.random_uniform_initializer)
+            self.policy = self._create_network()
 
             self.reward = self._get_reward()
             self.loss = self._get_loss()
@@ -24,32 +28,32 @@ class CommNet(BaseModel):
 
     def _create_network(self):
         # look-up table
-        look_up = tf.get_variable("look_up_table", shape=(self.num_agents, self.vector_len),
-                                  initializer=tf.random_normal_initializer)
 
+        input_one_hot = tf.one_hot(self.input, self.num_agents)
         # CommNet
-        h0 = tf.einsum("ijk,kl->ijl", self.input, look_up)
+        h0 = tf.einsum("ijk,kl->ijl", input_one_hot, self.look_up)
         h1 = self._create_cell("step_first", self.c_meta, h0, h0)
         c1 = self._mean(h1)
         h2 = self._create_cell("step_second", c1, h1, h0)
-
-        dense_weight = tf.get_variable("dense_weight", shape=(self.vector_len, self.n_actions))
-        out = tf.einsum("ijk,kl->ijl", h2, dense_weight)
+        out = tf.einsum("ijk,kl->ijl", h2, self.dense_weight)
 
         # soft-max
-        soft = tf.nn.softmax(out) + self.bias
+        soft = tf.nn.softmax(out)
 
         return soft
 
     def _sample_action(self):
         reshape_policy = tf.reshape(self.policy, shape=(-1, self.n_actions))
-        actions = tf.multinomial(tf.log(reshape_policy), num_samples=1)
-        one_hot = tf.one_hot(actions, depth=self.n_actions)
+        # sample actions
+        self.actions = tf.multinomial(tf.log(reshape_policy + self.bias), num_samples=1)
+        one_hot = tf.one_hot(self.actions, depth=self.n_actions)
         self.one_hot = tf.reshape(one_hot, shape=(-1, self.num_leaver, self.n_actions))
 
     def _get_reward(self):
         self._sample_action()
-        distinct_num = tf.reduce_sum(tf.cast(tf.reduce_sum(self.one_hot, axis=1) > 0, tf.float32), axis=1)
+        # self.reward_temp = tf.reduce_sum(self.one_hot, axis=1)
+        distinct_num = tf.reduce_sum(tf.cast(tf.reduce_sum(self.one_hot, axis=1) > 0, tf.float32), axis=1,
+                                     keep_dims=True)
         return distinct_num / self.num_leaver
 
     def _get_loss(self):
@@ -58,32 +62,36 @@ class CommNet(BaseModel):
 
         temp1 = -self.one_hot * tf.log(self.policy)
         temp2 = temp1 * tf.tile(meta, [1, 5, 5])
-        loss = tf.reduce_mean(tf.reduce_sum(temp2))
+        loss = tf.reduce_sum(temp2)
 
         return loss
 
-    def get_reward(self, ids_one_hot):
+    def get_reward(self, ids):
         # produce
         return self.sess.run(self.reward, feed_dict={
-            self.input: ids_one_hot,
+            self.input: ids,
             self.mask: self.mask_data,
             self.c_meta: np.zeros((self.batch_size, self.num_leaver, self.vector_len))
         })
 
-    def train(self, ids_one_hot, base_line, itr):
-        ids = np.array([np.random.choice(self.num_agents, 1, replace=False)
-                        for _ in range(self.batch_size)])
-        ids_one_hot = tf.one_hot(ids, self.vector_len)
+    def train(self, ids, base_line, itr):
 
-        _, loss, policy = self.sess.run([self.train_op, self.loss, self.policy], feed_dict={
-            self.input: ids_one_hot,
+        _, loss, ou = self.sess.run([self.train_op, self.loss, self.actions], feed_dict={
+            self.input: ids,
             self.mask: self.mask_data,
             self.c_meta: np.zeros((self.batch_size, self.num_leaver, self.vector_len)),
             self.base_line: base_line
         })
 
-        if (itr + 1) % 100 == 0:
+        if (itr + 1) % 10 == 0:
             print("loss of actor: ", loss)
+
+    def pred(self, ids):
+        return self.sess.run(self.reward, feed_dict={
+            self.input: ids,
+            self.mask: self.mask_data,
+            self.c_meta: np.zeros((1, self.num_leaver, self.vector_len))
+        })
 
 
 class BaseLine(BaseModel):
@@ -93,11 +101,12 @@ class BaseLine(BaseModel):
 
         self.n_actions = 1
         self.eta = 0.003
+        self.bias = 1e-4
 
-        self.reward = tf.placeholder(tf.float32, shape=(None, self.num_leaver, self.n_actions))
+        self.reward = tf.placeholder(tf.float32, shape=(None, 1))
 
         # ==== create network =====
-        with tf.variable_scope("CommNet"):
+        with tf.variable_scope("Arctic"):
             self.eval_name = tf.get_variable_scope().name
             self.baseline = self._create_network()  # n * 5 * n_actions
 
@@ -112,33 +121,36 @@ class BaseLine(BaseModel):
         look_up = tf.get_variable("look_up_table", shape=(self.num_agents, self.vector_len),
                                   initializer=tf.random_normal_initializer)
 
+        input_one_hot = tf.one_hot(self.input, self.num_agents)
         # CommNet-Baseline
-        h0 = tf.einsum("ijk,kl->ijl", self.input, look_up)
+        h0 = tf.einsum("ijk,kl->ijl", input_one_hot, look_up)
         h1 = self._create_cell("step_first", self.c_meta, h0, h0)
         c1 = self._mean(h1)
         h2 = self._create_cell("step_second", c1, h1, h0)
-        out = tf.layers.dense(h2, units=self.n_actions, use_bias=1e-4, activation=tf.nn.sigmoid())
+
+        dense_weight = tf.get_variable("dense_weight", shape=(self.vector_len, 1),
+                                       initializer=tf.random_normal_initializer)
+        dense = tf.einsum("ijk,kl->ijl", h2, dense_weight) / self.num_leaver
+
+        out = tf.reduce_mean(tf.sigmoid(dense), axis=1)
 
         return out
 
     def _get_loss(self):
-        loss = tf.reduce_mean(tf.reduce_sum(tf.square(self.baseline - self.reward))) * self.eta
+        loss = tf.reduce_sum(tf.square(self.baseline - self.reward)) * self.eta
         return loss
 
-    def get_reward(self, ids_one_hot):
+    def get_reward(self, ids):
         return self.sess.run(self.baseline, feed_dict={
-            self.input: ids_one_hot,
+            self.input: ids,
             self.mask: self.mask_data,
             self.c_meta: np.zeros((self.batch_size, self.num_leaver, self.vector_len))
         })
 
-    def train(self, ids_one_hot, reward, itr):
-        ids = np.array([np.random.choice(self.num_agents, 1, replace=False)
-                        for _ in range(self.batch_size)])
-        ids_one_hot = tf.one_hot(ids, self.vector_len)
+    def train(self, ids, reward, itr):
 
         _, loss = self.sess.run([self.train_op, self.loss], feed_dict={
-            self.input: ids_one_hot,
+            self.input: ids,
             self.mask: self.mask_data,
             self.c_meta: np.zeros((self.batch_size, self.num_leaver, self.vector_len)),
             self.reward: reward
--- train.py
+++ train.py
@@ -3,27 +3,32 @@ import numpy as np
 from commnet import BaseLine, CommNet
 
 
-N_AGENTS = 500
+N_AGENTS = 50
 VECTOR_LEN = 128
 BATCH_SIZE = 64
+LEVER = 5
 
 
 def train(episode):
     actor = CommNet(num_agents=N_AGENTS, vector_len=VECTOR_LEN, batch_size=BATCH_SIZE)
     critic = BaseLine(num_agents=N_AGENTS, vector_len=VECTOR_LEN, batch_size=BATCH_SIZE)
 
-    ids = np.array([np.random.choice(N_AGENTS, 1, replace=False)
-                    for _ in range(BATCH_SIZE)])
-    ids_one_hot = tf.one_hot(ids, VECTOR_LEN)
+    id_1 = np.array([np.random.choice(N_AGENTS, LEVER, replace=False)])
 
     for i in range(episode):
-        reward = actor.get_reward(ids_one_hot)
-        baseline = critic.get_reward(ids_one_hot)
+        ids = np.array([np.random.choice(N_AGENTS, LEVER, replace=False)
+                        for _ in range(BATCH_SIZE)])
 
-        actor.train(ids_one_hot, baseline, i)
-        critic.train(ids_one_hot, reward, i)
+        reward = actor.get_reward(ids)
+        baseline = critic.get_reward(ids)
+
+        actor.train(ids, baseline, i)
+        critic.train(ids, reward, i)
+
+        if (i + 1) % 10 == 0:
+            print(actor.pred(id_1))
 
 
 if __name__ == "__main__":
-    train(5000)
+    train(500)
 
